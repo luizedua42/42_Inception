fa492bd893f751f92eb99d896fde0ed7fd9faedb
luizedua42 <luizedua@student.42sp.org.br>
2024-04-01 12:07:07 -0300

    mariadb ok?

--- srcs/requirements/mariadb/conf/scriptDB.sh
+++ srcs/requirements/mariadb/conf/scriptDB.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`myFirstDB\`;"
-mysql -e "CREATE USER IF NOT EXISTS 'luizedua'@'localhost INDENTIFIED BY 'luizedua42';"
-mysql -e "GRANT ALL PRIVILEGES ON \`myFirstDB\`.* TO 'luizedua'@'localhost';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
-mysql -e "FLUSH ALL PRIVILEGES;"
-mysqladmin -u root -p 123 shutdown
+service mariadb start
+mariadb -e "CREATE DATABASE IF NOT EXISTS myFirstDB;"
+mariadb -e "CREATE USER IF NOT EXISTS 'luizedua'@'%' IDENTIFIED BY '123';"
+mariadb -e "GRANT ALL PRIVILEGES ON myFirstDB.* TO 'luizedua'@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
 exec mysqld_safe
