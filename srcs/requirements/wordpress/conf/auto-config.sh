d4998986c04e3c31866d9ebf85cc3fb0dbce33a7
luizedua42 <luizedua42@outlook.com>
2024-04-02 14:21:51 -0300

    eu to achando que vai hein

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,6 +1,26 @@
 #! /bin/bash
-cp wp-config-sample.php wp-config.php && \
-sed -i "s/database_name_here/wordpress_db/g" wp-config.php && \
-sed -i "s/username_here/wuser/g" wp-config.php && \
-sed -i "s/password_here/wpass/g" wp-config.php && \
-sed -i "s/localhost/mariadb/g" wp-config.php
+# rm -rf /var/www/wordpress/wp-config.php
+# touch /var/www/wordpress/wp-config.php
+# wp config create	--allow-root \
+# 	--dbname=$SQL_DATABASE \
+# 	--dbuser=$SQL_USER \
+# 	--dbpass=$SQL_PASSWORD \
+# 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+# php-fpm -F
+
+
+wp core install --allow-root \
+	--path=/var/www/wordpress \
+	--title="Inception" \
+	--url=$DOMAIN_NAME \
+	--admin_user=$WP_ROOT_USER \
+	--admin_password=$WP_ROOT_PASSWORD \
+	--admin_email=$WP_ROOT_EMAIL
+
+wp user create --allow-root	\
+	--path=/var/www/wordpress \
+	"$WP_USER" "$WP_EMAIL" \
+	--user_pass=$WP_PASSWORD \
+	--role='author'
+
+exec php-fpm -F
